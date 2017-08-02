4dd31a91fb461940db27b4f410ba9f9bac7f42f1
mantarfish <larawolf388@gmail.com>
2017-08-02 14:02:02 +1000

    added pythonanywhere to allowed host

--- tuts/tuts/settings.py
+++ tuts/tuts/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vpia+scio50c$pu$va3^9rrv(03o23%mek(0!!ob&+o(#8=e(9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mantarfish.pythonanywhere.com']
 
 
 # Application definition
