c41a00989202d201e6504a0f817425df197c5861
mantarfish <larawolf388@gmail.com>
2017-08-02 11:51:28 +1000

    My Django Girl App, first commit

---
+++ tuts/blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from blog.models import Post
+
+
+# Register your models here.
+# dummp_id = 'Mantarfish'
+# dummy_str = 'PP=8c3:)gcYp&6u2'
+admin.site.register(Post)
