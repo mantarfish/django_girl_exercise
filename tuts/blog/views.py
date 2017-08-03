aa996dd1323f0e5e176944aa105e87213eb40033
mantarfish <larawolf388@gmail.com>
2017-08-03 13:18:31 +1000

    detail page

--- tuts/blog/views.py
+++ tuts/blog/views.py
@@ -6,3 +6,7 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    posts = Post.objects.filter(pk=pk)
+    return render(request, 'blog/post_list.html', {'posts': posts})
