6c574a541b21caf9606ac9b6d0ec531773733f7c
mantarfish <larawolf388@gmail.com>
2017-08-04 17:15:28 +1000

    added form

--- tuts/blog/views.py
+++ tuts/blog/views.py
@@ -11,3 +11,10 @@ def post_list(request):
 def post_detail(request, pk):
     posts = Post.objects.filter(pk=pk)
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_new(request):
+    #new = [{'title': 'new', 'published_date': 'a_date', 'text': 'a_text'}]
+    #return render(request, 'blog/post_list.html', {'posts': new})
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
