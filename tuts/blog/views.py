b119eaf434b14204804d870848e1b3e9c2639c73
mantarfish <larawolf388@gmail.com>
2017-08-05 09:04:09 +1000

    added new post page

--- tuts/blog/views.py
+++ tuts/blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 from django.utils import timezone
 from .forms import PostForm
+
 
 # Create your views here.
 def post_list(request):
@@ -16,5 +17,17 @@ def post_detail(request, pk):
 def post_new(request):
     #new = [{'title': 'new', 'published_date': 'a_date', 'text': 'a_text'}]
     #return render(request, 'blog/post_list.html', {'posts': new})
-    form = PostForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            #message = 'posted'
+            #return render(request, 'blog/post_edit.html', {'message': message})
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
